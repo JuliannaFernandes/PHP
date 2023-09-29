2f6f647ed31a6811ce9a380697b2c0086e1641bd
Julianna Fernandes <juliannabf29@gmail.com>
2023-09-29 20:14:21 -0300

    case sensitive

---
+++ case_sensitive/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    //Não é case sensitive 
+    echo"teste <br>";
+    eCHO"teste2 <br>";
+    ECHO"TESTE <br>";
+    Echo"kjdhfksh <br>";
+
+
+    //Já variáveis SÃO case sensitive E
+    $nome="julianna você vai a partir de hoje estudar pra caralho até aprender a programar e conseguir um emprego na área de tecnologia ";
+
+
+    echo $nome;
+?>
+
+
