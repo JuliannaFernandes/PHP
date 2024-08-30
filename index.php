a8fadd6b00d245d331216fb10d25e2b6794f146c
Julianna Fernandes <juliannabf29@gmail.com>
2024-08-30 20:08:53 -0300

    feat: adicionando página para acessar atividades

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Curso PHP</title>
+    <style>
+       
+
+        a {
+            text-decoration: none;
+            color: black;
+        }
+
+        .navbar {
+            background-color: #87c3ff !important;
+            padding: 10px 0;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+
+<body>
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <h1>Curso PHP</h1>
+  </div>
+</nav>
+
+    <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+            Conteúdos
+        </a>
+        <a class="list-group-item list-group-item-action" href="/intro/index.php">1 - Introdução</a>
+        <a class="list-group-item list-group-item-action" href="/instrucao_de_codigo/index.php">2 - Variáveis</a>
+        <a class="list-group-item list-group-item-action" href="/espaco_em_branco/index.php">3 - Espaços em branco</a>
+        <a class="list-group-item list-group-item-action" href="/case_sensitive/index.php">4 - Case Sensitive</a>
+        <a class="list-group-item list-group-item-action" href="/checando_inteiro/index.php">5 - Checando inteiros</a>
+        <a class="list-group-item list-group-item-action" href="/testeConexao/index.php">6 - Teste Conexao</a>  
+        <a class="list-group-item list-group-item-action" href="/array/index.php">7 - Arrays</a>
+        <a class="list-group-item list-group-item-action" href="/blog/index.php">8 - Blog</a>
+        <a class="list-group-item list-group-item-action" href="/crud_php/index.php">9 - Crud</a>
+        <a class="list-group-item list-group-item-action" href="/funcoes/index.php">10 - Funções</a>
+        <a class="list-group-item list-group-item-action" href="/parametros_funcoes/index.php">11 - Parâmetros em funções</a>
+        <a class="list-group-item list-group-item-action" href="/return/index.php">12 - Return</a>
+    
+    </div>
+</body>
+
+</html>
