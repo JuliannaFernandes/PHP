5f18affdc83016b2d0b46ed75a4d601e38d596cd
Julianna Fernandes <juliannabf29@gmail.com>
2024-08-30 20:09:25 -0300

    feat: retorno de funções

---
+++ return/exercicio_35.php
@@ -0,0 +1,13 @@
+<?php
+
+function elevarAoQuadrado($numero){
+    return $numero ** 2;
+}
+
+echo elevarAoQuadrado(2) . "<br>";
+echo elevarAoQuadrado(3) . "<br>";
+echo elevarAoQuadrado(38) . "<br>";
+echo elevarAoQuadrado(4) . "<br>";
+echo elevarAoQuadrado(5) . "<br>";
+
+?>
---
+++ return/index.php
@@ -0,0 +1,25 @@
+<?php
+
+function soma ($a, $b) {
+    return $a + $b;
+}
+
+echo soma(1, 2);
+echo "<br>";
+echo soma(3, 4);
+echo "<br>";
+echo soma(5, 6);
+echo "<br>";
+echo soma(7, 8);
+
+$valor = soma(9, 10);
+echo "<br>";
+
+echo $valor;
+
+
+?>
+<hr>
+<a href="exercicio_35.php">Ir para exercício</a>
+
+
